8d40b56f745913080daa098204e201037825d5a1
Lionsworth <MStJohn82@gmail.com>
2023-09-22 23:28:34 -0700

    Camera following Player, fixed animations.

--- Scream_Jam_test/Assets/Scripts/PlayerMovement.cs
+++ Scream_Jam_test/Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,18 @@ using UnityEngine.InputSystem;
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float runSpeed = 10f;
+    [SerializeField] float jumpSpeed = 5f;
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator myAnimator;
+    CapsuleCollider2D myCapsuleCollider;
   
     void Start()
     {
         myRigidbody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
+        myCapsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
    
@@ -26,9 +29,18 @@ public class PlayerMovement : MonoBehaviour
 
     void OnMove(InputValue value)
     { 
-        moveInput = value.Get<Vector2>();
-        
+        moveInput = value.Get<Vector2>();   
     }
+
+    void OnJump(InputValue value)
+    {
+        if (!myCapsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; }
+        if(value.isPressed)
+        {
+            myRigidbody.velocity += new Vector2(0f, jumpSpeed);
+        }
+    }
+
     void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
