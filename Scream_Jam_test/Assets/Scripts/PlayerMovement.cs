836ac0e4c5e61c2e111a49adde08df73ff9bd6ee
Lionsworth <MStJohn82@gmail.com>
2023-09-22 18:06:49 -0700

    Setting up Player and platform
    Set up the platform, working on making the Player move correctly. I accidently reversed the animation.

---
+++ Scream_Jam_test/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float runSpeed = 10f;
+
+    Vector2 moveInput;
+    Rigidbody2D myRigidbody;
+  
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+   
+    void Update()
+    {
+        Run();
+        FlipSprite();
+    }
+
+    void OnMove(InputValue value)
+    { 
+        moveInput = value.Get<Vector2>();
+        
+    }
+    void Run()
+    {
+        Vector2 playerVelocity = new Vector2(moveInput.x * runSpeed, myRigidbody.velocity.y);
+        myRigidbody.velocity = playerVelocity;
+    }
+
+    void FlipSprite()
+    {
+        transform.localScale = new Vector2(Mathf.Sign(myRigidbody.velocity.x), 1f);
+    }
+}
